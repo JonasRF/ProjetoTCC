a9ee42f4fda1478c6f540e35f19a24f92764c7eb
JonasRF <jonasribeiro41@yahoo.com.br>
2025-08-11 20:42:03 -0300

    Configing profiles with springboot, datasource, test,dev and production, hikari and flyway

--- admin-do-catalogo/infrastructure/src/main/java/org/jonasribeiro/admin/catalogo/infraestructure/Main.java
+++ admin-do-catalogo/infrastructure/src/main/java/org/jonasribeiro/admin/catalogo/infraestructure/Main.java
@@ -6,11 +6,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.jetty.JettyWebServer;
 import org.springframework.boot.web.server.WebServer;
+import org.springframework.core.env.AbstractEnvironment;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         System.out.printf("Hello, %s!%n", "World");
+        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "production");
         SpringApplication.run(WebServerConfig.class, args);
     }
 }
---
+++ admin-do-catalogo/infrastructure/src/test/java/org.jonasribeiro.admin.catalogo.infraestructure/MainTest.java
@@ -0,0 +1,13 @@
+package org.jonasribeiro.admin.catalogo.infraestructure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.core.env.AbstractEnvironment;
+
+public class MainTest {
+
+    @Test
+    public void testMain() {
+        System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "test");
+        Main.main(new String[]{});
+    }
+}
