ed150bd56d707fd99460e2a84019dca2754686a4
JonasRF <jonasribeiro41@yahoo.com.br>
2025-08-11 23:34:24 -0300

    creating category gateway implementation

---
+++ admin-do-catalogo/infrastructure/src/main/java/org/jonasribeiro/admin/catalogo/infraestructure/category/CategoryMySQLGateway.java
@@ -0,0 +1,46 @@
+package org.jonasribeiro.admin.catalogo.infraestructure.category;
+
+import org.jonasribeiro.admin.catalogo.domain.category.Category;
+import org.jonasribeiro.admin.catalogo.domain.category.CategoryGateway;
+import org.jonasribeiro.admin.catalogo.domain.category.CategoryID;
+import org.jonasribeiro.admin.catalogo.domain.category.CategorySearchQuery;
+import org.jonasribeiro.admin.catalogo.domain.pagination.Pagination;
+import org.jonasribeiro.admin.catalogo.infraestructure.category.persistence.CategoryRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CategoryMySQLGateway implements CategoryGateway {
+
+    private final CategoryRepository repository;
+
+    public CategoryMySQLGateway(CategoryRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public Category create(Category aCategory) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(CategoryID anId) {
+
+    }
+
+    @Override
+    public Optional<Category> findById(CategoryID anId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Category update(Category aCategory) {
+        return null;
+    }
+
+    @Override
+    public Pagination<Category> findAll(CategorySearchQuery aQuery) {
+        return null;
+    }
+}
