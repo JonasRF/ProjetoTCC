fa4ccadc3a961077d2d59753a4f1e0647cef6209
JonasRF <jonasribeiro41@yahoo.com.br>
2025-10-14 16:17:19 -0300

    Configurando o rabbitmq com docker, configuração manual dos recursos do rabbitmq, inclusão da biblioteca do rabbitmq ao projeto

---
+++ admin-do-catalogo/infrastructure/src/main/java/org/jonasribeiro/admin/catalogo/infraestructure/configuration/annotations/VideoCreatedQueue.java
@@ -0,0 +1,13 @@
+package org.jonasribeiro.admin.catalogo.infraestructure.configuration.annotations;
+
+import java.lang.annotation.Retention;
+import org.springframework.beans.factory.annotation.Qualifier;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Qualifier("VideoCreatedQueue")
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD})
+public @interface VideoCreatedQueue {
+}
